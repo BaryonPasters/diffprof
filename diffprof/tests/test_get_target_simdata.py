c2f29034d5764a55614d182786ca9569a716423c
Andrew Hearin <ahearin@anl.gov>
2021-10-12 15:05:16 -0500

    Removed param_scan dependency by porting in latin_hypercube_sampler

--- diffprof/tests/test_get_target_simdata.py
+++ diffprof/tests/test_get_target_simdata.py
@@ -1,33 +1,32 @@
 """
 """
 import numpy as np
-from ..get_target_simdata import target_data_generator, HAS_LHS
+from ..get_target_simdata import target_data_generator
 
 
 def test_target_data_generator():
-    if HAS_LHS:
-        n_bpl, n_mdpl2 = 5000, 5000
-        n_times = 15
-        n_mh_out, n_p_out = 12, 14
-        args = (
-            np.linspace(11, 15, n_bpl),
-            np.linspace(11, 15, n_mdpl2),
-            np.ones((n_bpl, n_times)),
-            np.ones((n_mdpl2, n_times)),
-            np.random.normal(0, 1, n_bpl),
-            np.random.normal(0, 1, n_mdpl2),
-            n_mh_out,
-            n_p_out,
-        )
-        gen = target_data_generator(*args)
-        target_data = next(gen)
-        lgmhalo_targets, p50_targets = target_data[0:2]
-        lgc_mean_targets_lgm0, lgc_std_targets_lgm0 = target_data[2:4]
-        lgc_mean_targets_lgm0_p50, lgc_std_targets_lgm0_p50 = target_data[4:]
+    n_bpl, n_mdpl2 = 5000, 5000
+    n_times = 15
+    n_mh_out, n_p_out = 12, 14
+    args = (
+        np.linspace(11, 15, n_bpl),
+        np.linspace(11, 15, n_mdpl2),
+        np.ones((n_bpl, n_times)),
+        np.ones((n_mdpl2, n_times)),
+        np.random.normal(0, 1, n_bpl),
+        np.random.normal(0, 1, n_mdpl2),
+        n_mh_out,
+        n_p_out,
+    )
+    gen = target_data_generator(*args)
+    target_data = next(gen)
+    lgmhalo_targets, p50_targets = target_data[0:2]
+    lgc_mean_targets_lgm0, lgc_std_targets_lgm0 = target_data[2:4]
+    lgc_mean_targets_lgm0_p50, lgc_std_targets_lgm0_p50 = target_data[4:]
 
-        assert lgmhalo_targets.size == n_mh_out
-        assert p50_targets.size == n_p_out
-        assert lgc_mean_targets_lgm0.shape == (n_mh_out, n_times)
-        assert lgc_std_targets_lgm0.shape == (n_mh_out, n_times)
-        assert lgc_mean_targets_lgm0_p50.shape == (n_mh_out, n_p_out, n_times)
-        assert lgc_std_targets_lgm0_p50.shape == (n_mh_out, n_p_out, n_times)
+    assert lgmhalo_targets.size == n_mh_out
+    assert p50_targets.size == n_p_out
+    assert lgc_mean_targets_lgm0.shape == (n_mh_out, n_times)
+    assert lgc_std_targets_lgm0.shape == (n_mh_out, n_times)
+    assert lgc_mean_targets_lgm0_p50.shape == (n_mh_out, n_p_out, n_times)
+    assert lgc_std_targets_lgm0_p50.shape == (n_mh_out, n_p_out, n_times)
