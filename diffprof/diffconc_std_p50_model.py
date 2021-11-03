c5c432def0ffebb178fc354afd21fa5075b6b890
Andrew Hearin <ahearin@anl.gov>
2021-11-03 07:16:07 -0500

    Updated p50-dependent scatter model

--- diffprof/diffconc_std_p50_model.py
+++ diffprof/diffconc_std_p50_model.py
@@ -8,7 +8,10 @@ from collections import OrderedDict
 PARAMS = OrderedDict(
     p50_sig_x0=0.5,
     p50_sig_k=5.0,
-    p50_sig_plo=0.05,
+    p50_sig_plo_x0=13,
+    p50_sig_plo_k=1,
+    p50_sig_plo_ylo=0.0459,
+    p50_sig_plo_yhi=0.0459,
     p50_sig_width_x0=13,
     p50_sig_width_k=1,
     p50_sig_width_ylo=0.05,
@@ -22,7 +25,10 @@ def _scatter_vs_p50_and_lgmhalo(
     p50,
     p50_sig_x0,
     p50_sig_k,
-    p50_sig_plo,
+    p50_sig_plo_x0,
+    p50_sig_plo_k,
+    p50_sig_plo_ylo,
+    p50_sig_plo_yhi,
     p50_sig_width_x0,
     p50_sig_width_k,
     p50_sig_width_ylo,
@@ -31,8 +37,19 @@ def _scatter_vs_p50_and_lgmhalo(
     width = _scatter_p50_width_vs_lgmhalo(
         lgmh, p50_sig_width_x0, p50_sig_width_k, p50_sig_width_ylo, p50_sig_width_yhi
     )
-
+    p50_sig_plo = _get_p50_sig_plo(
+        lgmh, p50_sig_plo_x0, p50_sig_plo_k, p50_sig_plo_ylo, p50_sig_plo_yhi
+    )
     return _sigmoid(p50, p50_sig_x0, p50_sig_k, p50_sig_plo, p50_sig_plo + width)
+
+
+@jjit
+def _get_p50_sig_plo(
+    lgmh, p50_sig_plo_x0, p50_sig_plo_k, p50_sig_plo_ylo, p50_sig_plo_yhi
+):
+    return _sigmoid(
+        lgmh, p50_sig_plo_x0, p50_sig_plo_k, p50_sig_plo_ylo, p50_sig_plo_yhi
+    )
 
 
 @jjit
