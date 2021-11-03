b2d9c1267de0df51c798ffdf2ff1718707899073
Andrew Hearin <ahearin@anl.gov>
2021-11-03 06:29:01 -0500

    Ported in p50-dependent model for scatter

---
+++ diffprof/diffconc_std_p50_model.py
@@ -0,0 +1,54 @@
+"""
+"""
+from jax import numpy as jnp
+from jax import jit as jjit
+from collections import OrderedDict
+
+
+PARAMS = OrderedDict(
+    p50_sig_x0=0.5,
+    p50_sig_k=5.0,
+    p50_sig_plo=0.05,
+    p50_sig_width_x0=13,
+    p50_sig_width_k=1,
+    p50_sig_width_ylo=0.05,
+    p50_sig_width_yhi=0,
+)
+
+
+@jjit
+def _scatter_vs_p50_and_lgmhalo(
+    lgmh,
+    p50,
+    p50_sig_x0,
+    p50_sig_k,
+    p50_sig_plo,
+    p50_sig_width_x0,
+    p50_sig_width_k,
+    p50_sig_width_ylo,
+    p50_sig_width_yhi,
+):
+    width = _scatter_p50_width_vs_lgmhalo(
+        lgmh, p50_sig_width_x0, p50_sig_width_k, p50_sig_width_ylo, p50_sig_width_yhi
+    )
+
+    return _sigmoid(p50, p50_sig_x0, p50_sig_k, p50_sig_plo, p50_sig_plo + width)
+
+
+@jjit
+def _scatter_p50_width_vs_lgmhalo(
+    lgmh,
+    p50_sig_width_x0,
+    p50_sig_width_k,
+    p50_sig_width_ylo,
+    p50_sig_width_yhi,
+):
+    return _sigmoid(
+        lgmh, p50_sig_width_x0, p50_sig_width_k, p50_sig_width_ylo, p50_sig_width_yhi
+    )
+
+
+@jjit
+def _sigmoid(x, tp, k, ylo, yhi):
+    height_diff = yhi - ylo
+    return ylo + height_diff / (1 + jnp.exp(-k * (x - tp)))
