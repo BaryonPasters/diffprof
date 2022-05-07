37da8e88915810228f53c956f3c1e7f879512eb7
Andrew Hearin <ahearin@anl.gov>
2022-05-07 16:05:59 -0500

    Cleaned up test

--- diffprof/tests/test_dpp.py
+++ diffprof/tests/test_dpp.py
@@ -1,6 +1,5 @@
 """
 """
-import pytest
 from ..bpl_dpp import DEFAULT_PARAMS, PARAMS_LBOUNDS, PARAMS_UBOUNDS, PARAM_BOUNDS
 
 
@@ -10,23 +9,7 @@ def test_lbounds_are_less_than_ubounds():
         assert PARAMS_LBOUNDS[key] < PARAMS_UBOUNDS[key], msg.format(key)
 
 
-@pytest.mark.skip
-def test_defaults_are_larger_than_lbounds():
-    msg = "default `{0}`= {1} < PARAMS_LBOUNDS[{0}]={2}"
-    for key, default in DEFAULT_PARAMS.items():
-        lbound = PARAMS_LBOUNDS[key]
-        assert lbound < default, msg.format(key, default, lbound)
-
-
-@pytest.mark.skip
-def test_defaults_are_less_than_ubounds():
-    msg = "default `{0}`= {1} > PARAMS_UBOUNDS[{0}]={2}"
-    for key, default in DEFAULT_PARAMS.items():
-        ubound = PARAMS_UBOUNDS[key]
-        assert ubound > default, msg.format(key, default, ubound)
-
-
-def test_defaults_are_bouned():
+def test_defaults_are_bounded():
     lo_msg = "default `{0}`= {1} < PARAMS_LBOUNDS[{0}]={2}"
     hi_msg = "default `{0}`= {1} > PARAMS_UBOUNDS[{0}]={2}"
     for key, default in DEFAULT_PARAMS.items():
