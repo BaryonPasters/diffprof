a2010adc14a32e0297ffcc329e45d9eaba23e728
Andrew Hearin <ahearin@anl.gov>
2022-05-07 16:00:09 -0500

    Fixed PARAM_BOUNDS dict

--- diffprof/tests/test_dpp.py
+++ diffprof/tests/test_dpp.py
@@ -1,6 +1,7 @@
 """
 """
-from ..bpl_dpp import DEFAULT_PARAMS, PARAMS_LBOUNDS, PARAMS_UBOUNDS
+import pytest
+from ..bpl_dpp import DEFAULT_PARAMS, PARAMS_LBOUNDS, PARAMS_UBOUNDS, PARAM_BOUNDS
 
 
 def test_lbounds_are_less_than_ubounds():
@@ -9,15 +10,27 @@ def test_lbounds_are_less_than_ubounds():
         assert PARAMS_LBOUNDS[key] < PARAMS_UBOUNDS[key], msg.format(key)
 
 
+@pytest.mark.skip
 def test_defaults_are_larger_than_lbounds():
     msg = "default `{0}`= {1} < PARAMS_LBOUNDS[{0}]={2}"
     for key, default in DEFAULT_PARAMS.items():
         lbound = PARAMS_LBOUNDS[key]
-        assert PARAMS_LBOUNDS[key] < default, msg.format(key, default, lbound)
+        assert lbound < default, msg.format(key, default, lbound)
 
 
+@pytest.mark.skip
 def test_defaults_are_less_than_ubounds():
     msg = "default `{0}`= {1} > PARAMS_UBOUNDS[{0}]={2}"
     for key, default in DEFAULT_PARAMS.items():
         ubound = PARAMS_UBOUNDS[key]
-        assert PARAMS_LBOUNDS[key] < default, msg.format(key, default, ubound)
+        assert ubound > default, msg.format(key, default, ubound)
+
+
+def test_defaults_are_bouned():
+    lo_msg = "default `{0}`= {1} < PARAMS_LBOUNDS[{0}]={2}"
+    hi_msg = "default `{0}`= {1} > PARAMS_UBOUNDS[{0}]={2}"
+    for key, default in DEFAULT_PARAMS.items():
+        lbound = PARAM_BOUNDS[key][0]
+        ubound = PARAM_BOUNDS[key][1]
+        assert lbound < default, lo_msg.format(key, default, lbound)
+        assert ubound > default, hi_msg.format(key, default, ubound)
