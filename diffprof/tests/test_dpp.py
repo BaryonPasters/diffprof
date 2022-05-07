d93f2e0b8f3db727fea6b66858aae11800af7070
Andrew Hearin <ahearin@anl.gov>
2022-05-07 16:07:23 -0500

    Removed unnecessary tests

--- diffprof/tests/test_dpp.py
+++ diffprof/tests/test_dpp.py
@@ -1,17 +1,11 @@
 """
 """
-from ..bpl_dpp import DEFAULT_PARAMS, PARAMS_LBOUNDS, PARAMS_UBOUNDS, PARAM_BOUNDS
-
-
-def test_lbounds_are_less_than_ubounds():
-    msg = "`{0}` is improperly bounded by PARAMS_LBOUNDS and PARAMS_UBOUNDS"
-    for key in DEFAULT_PARAMS.keys():
-        assert PARAMS_LBOUNDS[key] < PARAMS_UBOUNDS[key], msg.format(key)
+from ..bpl_dpp import DEFAULT_PARAMS, PARAM_BOUNDS
 
 
 def test_defaults_are_bounded():
-    lo_msg = "default `{0}`= {1} < PARAMS_LBOUNDS[{0}]={2}"
-    hi_msg = "default `{0}`= {1} > PARAMS_UBOUNDS[{0}]={2}"
+    lo_msg = "default `{0}`= {1} < lower bound = {2}"
+    hi_msg = "default `{0}`= {1} > upper bound = {2}"
     for key, default in DEFAULT_PARAMS.items():
         lbound = PARAM_BOUNDS[key][0]
         ubound = PARAM_BOUNDS[key][1]
